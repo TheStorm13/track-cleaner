45e0155a2a8f047825e9981a6706acb1a0d2aedc
ilya-groza <ilya.groza13@gmail.com>
2025-06-20 20:01:02 +0400

    feat: add GpxUtils class for distance calculation and GPX segment creation

---
+++ src/utils/gpx_utils.py
@@ -0,0 +1,57 @@
+from math import radians, sin, cos, sqrt, asin
+
+import gpxpy
+
+
+class GpxUtils:
+
+    @staticmethod
+    def distance_between_points(point1: gpxpy.gpx.GPXTrackPoint,
+                                point2: gpxpy.gpx.GPXTrackPoint
+                                ) -> float:
+        """Расчет расстояния между двумя точками (в метрах)
+
+        Args:
+            point1: Первая точка (широта и долгота).
+            point2: Вторая точка (широта и долгота).
+
+        """
+
+        # Преобразование градусов в радианы
+        lat1, lon1 = radians(point1.latitude), radians(point1.longitude)
+        lat2, lon2 = radians(point2.latitude), radians(point2.longitude)
+
+        # Разница широт и долгот
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+
+        # Формула гаверсинуса
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+        # Расстояние в метрах (средний радиус Земли)
+        EARTH_RADIUS_METERS = 6_371_000
+        distance = EARTH_RADIUS_METERS * 2 * asin(sqrt(a))
+
+        return distance
+
+    @staticmethod
+    def create_gpx(i: int, j: int, points):
+        """
+        Создает новый GPX объект с сегментом, содержащим точки от i до j.
+
+        Args:
+            i: Начальный индекс сегмента.
+            j: Конечный индекс сегмента.
+            points: Список точек, из которых будет создан сегмент.
+
+        Returns:
+
+        """
+        segment_points = points[i:j + 1]
+        gpx_bad = gpxpy.gpx.GPX()
+        track = gpxpy.gpx.GPXTrack()
+        gpx_bad.tracks.append(track)
+        segment = gpxpy.gpx.GPXTrackSegment()
+        track.segments.append(segment)
+        segment.points.extend(segment_points)
+        return gpx_bad
