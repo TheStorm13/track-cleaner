fe6deb37c93757cd56812f40cda50a5fdc0af17e
ilya-groza <ilya.groza13@gmail.com>
2025-06-20 19:59:46 +0400

    feat: add configuration for track simplification and loop parameters

---
+++ config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent
+
+# Упрощение трека
+TRACK_SIMPLIFICATION_TOLERANCE_M = 10.0
+
+# Небольшие замкнутого участки
+MIN_SMALL_LOOP_LENGTH_M = 50.0
+MAX_SMALL_LOOP_LENGTH_M = 1000.0
+
+# Замкнутые участки
+MIN_CLOSED_LOOP_LENGTH_M = 50.0
+MAX_CLOSED_LOOP_LENGTH_M = 1_000.0
+LOOP_CLOSURE_THRESHOLD_M = 25.0
